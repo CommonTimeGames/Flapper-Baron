67b9efa0475e1e113972c47a6832ef00713c1f94
Nigel Patrick Brady <nbrady@ciis.canon.com>
2014-04-26 22:16:58 -0400

    Initial project import, with correctly-configured Gradle builds!

---
+++ android/src/com/commontimegames/flapper/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.commontimegames.flapper.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.commontimegames.flapper.FlapperGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlapperGame(), config);
+	}
+}
